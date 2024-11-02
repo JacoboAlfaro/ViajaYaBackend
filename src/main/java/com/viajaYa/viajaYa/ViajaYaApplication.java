4d4a001929c464ba837abf2570eb681600622add
JacoboAlfaro <jacobo.alfaro06@gmail.com>
2024-11-02 15:55:40 -0500

    Initial commit

---
+++ src/main/java/com/viajaYa/viajaYa/ViajaYaApplication.java
@@ -0,0 +1,14 @@
+package com.viajaYa.viajaYa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViajaYaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ViajaYaApplication.class, args);
+		System.out.println("Proyecto viajaYA");
+	}
+
+}
---
+++ src/test/java/com/viajaYa/viajaYa/ViajaYaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.viajaYa.viajaYa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ViajaYaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
